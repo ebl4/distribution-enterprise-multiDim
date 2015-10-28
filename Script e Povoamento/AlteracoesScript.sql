a26f1c5dc281b7f4821bde0a89bc0f8ed18d6700
gcpa <gcpa2404@hotmail.com>
2015-10-28 00:29:25 -0300

    Update AlteracoesScript.sql

--- Script e Povoamento/AlteracoesScript.sql
+++ Script e Povoamento/AlteracoesScript.sql
@@ -50,3 +50,20 @@ WHERE Preco BETWEEN 8000 AND 9000;
 UPDATE Produto
 SET Categoria = '9'
 WHERE Preco BETWEEN 9000 AND 10000;
+
+ALTER TABLE Produto
+ADD CategoriaPrecos nvarchar(20)
+
+GO
+
+UPDATE Produto
+SET CategoriaPrecos = 'Barato'
+WHERE Preco < 3000;
+
+UPDATE Produto
+SET CategoriaPrecos = 'Mediano'
+WHERE Preco BETWEEN 3000 AND 7000;
+
+UPDATE Produto
+SET CategoriaPrecos = 'Caro'
+WHERE Preco > 7000;
